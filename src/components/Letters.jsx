19edc787d50fd5515919670eb66db9e653cc6486
AnshumanDhiman <67755381+AnshumanDhiman@users.noreply.github.com>
2023-03-21 14:11:41 +0530

    changes

--- src/components/Letters.jsx
+++ src/components/Letters.jsx
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
+import { Data } from "../data";
 
 const Letters = () => {
-  const [reply1, setReply1] = useState("");
-  const [reply2, setReply2] = useState("");
-  const [reply3, setReply3] = useState("");
-  const [visible, setVisible] = useState(false);
-  const conversations = [
-    {
-      message1:
-        "I am a 45 year old man living with my wife, son, daughter, mother and father. For the past 7 months, we’ve been trying to get electricity connections from the electricity department but each time, we’re being asked to pay a fee. Please help our community get electricity connections. All our children will be able to study better.",
-      reply1: reply1,
-      message2:
-        "I’ve discovered concerning activity in the electricity department. Requesting an appointment to meet you as soon as possible",
-      reply2: reply2,
-      message3:
-        "Hello Sir! I want to set up a cinema business in the state. My mother is visiting a relative here. She has asked me to talk to you.",
-      reply3: reply3,
-    },
-  ];
+
+
+  const [visible, setVisible] = useState(true);
+  const [message, setMessage] = useState('');
+  const handleChange = event => {
+    setMessage(event.target.value);
+  };
+  // const handleClick = event => {
+  //   event.preventDefault();
+
+  //   if (message.trim().length !== 0) {
+  //     console.log('input value is NOT empty');
+  //     //change window location
+  //     setVisible(true);
+  //     console.log(visible);
+  //   } else {
+  //     setVisible(false);
+  //     console.log('input value is empty');
+  //   }
+
+    
+  // };
+
+
+
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content ">
@@ -26,47 +35,53 @@ const Letters = () => {
             <div className="card w-96 bg-base-100 shadow-xl">
               <div className="card-body">
                 <h2 className="card-title">Dear Sir/Ma'am</h2>
-                <p>{conversations[0].message1}</p>
+                    {Data.map((u) => (                
+                  <div>
+                    <p className="card-subtitle">{u.message1}</p>
+                  </div>
+                    ))}          
                 <div className="card-actions justify-end">
                   {/* The button to open modal */}
                   <label htmlFor="my-modal-3" className="btn btn-ghost">
-                    Close
+                    <a href="/tasks">Close</a>
                   </label>
                   {/* Put this part before </body> tag */}
-
-                  <button
-                    className="btn btn-primary"
-                    onClick={() => setVisible(true)}
-                  >
-                    Reply
-                  </button>
-                </div>
-                <input
-                  type="checkbox"
-                  id="my-modal-3"
-                  className="modal-toggle"
-                />
-                <div className="modal">
-                  <div className="modal-box relative">
-                    <label
-                      htmlFor="my-modal-3"
-                      className="btn btn-sm btn-circle absolute right-2 top-2"
-                    >
-                      ✕
-                    </label>
-                    <h3 className="text-lg font-bold">Warning!!</h3>
-                    <p className="py-4">
-                      Aspirants love to deal with the complexities of the real
-                      world. The best way to kick-start your preparation is to
-                      understand the complexities that a bureaucrat faces in
-                      dealing with day to day problems of society
-                    </p>
+                  <label htmlFor="my-modal" className="btn btn-primary">Reply</label>
+                  
+                 
+                  {visible ? (
+                    <>
+                    <input type="checkbox" id="my-modal" className="modal-toggle" />
+                    <div className="modal">
+                      <div className="modal-box">
+                        <h3 className="font-bold text-lg">Dear Sir,</h3>
+                        <p className="py-4">I’ve discovered concerning activity in the electricity department. Requesting an appointment to meet you as soon as possible</p>
+                        <div className="modal-action">
+                        <label htmlFor="my-modal" className="btn">Ok</label>
+                        
+                     </div>
+                    </div>
                   </div>
+                  </>
+                  )
+                  : (
+                    <>
+                    <input type="checkbox" id="my-modal" className="modal-toggle" />
+                    <div className="modal">
+                      <div className="modal-box">
+                        <h3 className="font-bold text-lg">Empty Reply!</h3>
+                        <p className="py-4">Please enter a reply</p>
+                        <div className="modal-action">
+                        <label htmlFor="my-modal" className="btn">Ok</label>
+                      </div>
+                    </div>
+                  </div>
+                    </>
+                  )}
+
                 </div>
-                <textarea
-                  className={`textarea , ${visible ? "block" : "hidden"}`}
-                  placeholder="Reply"
-                ></textarea>
+                  <textarea id="my-modal-3" onChange={handleChange} autoComplete="off" placeholder="Reply"></textarea>
+                
               </div>
             </div>
           </div>
---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const Data = [
+    {
+      id: 1,
+      message1: "I am a 45 year old man living with my wife, son, daughter, mother and father. For the past 7 months, we’ve been trying to get electricity connections from the electricity department but each time, we’re being asked to pay a fee. Please help our community get electricity connections. All our children will be able to study better.",
+      reply: "",
+      status: "unread",
+    },
+    {
+      id: 2,
+      message: "I’ve discovered concerning activity in the electricity department. Requesting an appointment to meet you as soon as possible",
+      reply: "",
+      status: "unread",
+    },
+    {
+      id: 3,
+      message: "Hello Sir! I want to set up a cinema business in the state. My mother is visiting a relative here. She has asked me to talk to you.",
+      reply: "",
+      status: "unread",
+    }
+  ]
